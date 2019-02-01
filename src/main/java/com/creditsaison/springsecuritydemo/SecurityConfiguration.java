c9d0c3e98ce684f2c7260c84552fd330c7374287
Gourav Suri <gourav96suri@yahoo.com>
2019-02-01 12:39:28 +0530

    Implemented Spring Access Control using Spring Security Framework

---
+++ src/main/java/com/creditsaison/springsecuritydemo/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.creditsaison.springsecuritydemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication().withUser("bill").password(encoder.encode("abc123")).roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("root123")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("dba").password(encoder.encode("root123")).roles("ADMIN","DBA");//dba have two roles.
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                .and().formLogin()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+
+    }
+}
